81071dfa4dc99f5e668a5647fae1bae91fc26d5f
BitWitchery <vicfei.vr@googlemail.com>
2024-09-10 22:17:17 +0200

    Add files via upload

---
+++ Day 004/index.py
@@ -0,0 +1,15 @@
+fruits = ["Mango", "Apple", "Pear"]
+
+# counts items in list
+new_fruits = len(fruits)
+print(new_fruits)
+
+# shows items (min -1 is important)
+print(fruits[new_fruits - 1])
+
+# list in a list
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
